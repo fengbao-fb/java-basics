33cbc57b0d57880e2fcb31c4cf046e4a54fc4ddd
HuangHao <1755624992@qq.com>
2025-05-12 16:32:26 +0800

    init

---
+++ java-basics-example/src/main/java/hh/javabasicsexample/JavaBasicsExampleApplication.java
@@ -0,0 +1,13 @@
+package hh.javabasicsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicsExampleApplication.class, args);
+    }
+
+}
